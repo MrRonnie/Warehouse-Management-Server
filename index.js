26fab10f214641132e537a1843717b8e77a7bf9c
MrRonnie <ronymaza55@gmail.com>
2022-05-09 22:35:18 +0600

    Connect to database with secure password on environment variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 // database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.99ubt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("Gadget Monkey DB Connected");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Running Warehouse Server");
 });
