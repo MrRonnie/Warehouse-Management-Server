be36f33e6a3eec2fccc42505450d89d985711c91
MrRonnie <ronymaza55@gmail.com>
2022-05-10 15:22:32 +0600

    POST data from client side and added in mongodb cloud database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,19 @@ async function run() {
       const cursor = itemsCollection.find(query);
       const items = await cursor.toArray();
       res.send(items);
+    });
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemsCollection.findOne(query);
+      res.send(item);
+    });
+
+    // Post
+    app.post("/item", async (req, res) => {
+      const newItem = req.body;
+      const result = await itemsCollection.insertOne(newItem);
+      res.send(result);
     });
   } finally {
   }
